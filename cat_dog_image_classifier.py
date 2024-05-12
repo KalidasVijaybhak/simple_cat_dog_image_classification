46af1d4bc9f50d721c947bf7add7a2fd1f731308
Kalidas <calidazvb@gmail.com>
2024-05-12 13:43:31 +0530

    basic model set

---
+++ cat_dog_image_classifier.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import keras
+
+st.set_page_config(
+    page_title="CAT DOG IMAGE CLASSIFIER",
+    page_icon=":dog:",
+    layout="wide",
+)
+# https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
+st.title("CAT DOG Image Classifer")
+st.write("check out the [Model](https://www.kaggle.com/code/kalidasvijaybhak/cat-and-dog-classification)")
+if "file_uploader_key" not in st.session_state:
+    st.session_state["file_uploader_key"] = 0
+img_file_buffer = st.file_uploader('Upload a JPG image', type='jpg', key=st.session_state["file_uploader_key"], accept_multiple_files=False)
+target_size = (150, 150)
+
+loaded_model = keras.models.load_model('simple_cnn_cat_dog.h5')
+
+# File uploader and image display on the left
+col1, col2 = st.columns([3, 2])
+
+with col1:
+
+    if img_file_buffer is not None:
+        img1 = Image.open(img_file_buffer)
+        img2 = Image.open(img_file_buffer)
+        new_image = img2.resize((600, 400))
+        st.image(new_image, caption=img_file_buffer.name)
+        img = np.array(img1)
+        if len(img.shape) != 3:
+            raise ValueError("Invalid image shape")
+        img = tf.image.resize(img, target_size)
+        img = img / 255
+        img_array = img.numpy()
+
+# Buttons on the right
+with col2:
+    if st.button('Refresh Data'):
+        st.session_state["file_uploader_key"] += 1
+        st.experimental_rerun()
+    
+    if st.button('Classify'):
+        if img_file_buffer is not None:
+            y_pred = loaded_model.predict(img_array.reshape(1, 150, 150, 3))
+            output = "dog" if y_pred > 0.5 else "cat"
+            st.header("This is a " + output)
+        elif img_file_buffer is None:
+            st.warning('Upload an image first', icon="⚠️")
+ 
